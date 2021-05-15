8c3ce3064fd90e6f48ab39412944c3b7258fb1d1
Zachary Allen <zac_allen@Apples-MacBook-Pro.local>
2021-05-15 12:09:43 +0200

    initial commit, reverse a string

---
+++ Main.java
@@ -0,0 +1,21 @@
+class Main {
+    public static void main(String[] args) {
+
+        // Challenge 1
+        // Given an input string, reverse the string word by word.
+        // A word is defined as a sequence of non space characters.
+        // The input string does not contain leading or traling spaces and the words are always separated by a single space.
+
+        // Input: "Leonardo da Vinci"
+        // Expected Output: "Vinci da Leonardo"
+        String inputString[] = "Leanardo da Vinci".split(" ");
+        String reversedString = "";
+
+        for (int word = inputString.length - 1; word >= 0; word--) {
+            reversedString += inputString[word] + " ";
+        }
+
+        System.out.print(reversedString.substring(0, reversedString.length() -1));
+
+    }
+}

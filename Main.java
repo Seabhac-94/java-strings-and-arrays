695eb30ea58eebc4cc622ba50b08040581abf259
Zac Allen <zac.allen@outlook.com>
2021-05-16 16:42:21 +0200

    moved challenges into separate repositiories

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main {
     public static void main(String[] args) {
 
-        // Challenge 1
+        // Challenge
         // Given an input string, reverse the string word by word.
         // A word is defined as a sequence of non space characters.
         // The input string does not contain leading or traling spaces and the words are always separated by a single space.
@@ -15,44 +15,5 @@ class Main {
             reversedString += inputString[word] + " ";
         }
         System.out.println(reversedString.substring(0, reversedString.length() -1));
-
-        // Challenge 2
-        // Rotate an array of n elements to the left by k steps
-        // Input [1, 2, 3, 4, 5, 6, 7]
-        // Expected output [4, 5, 6, 7, 1, 2, 3]
-
-        RotateArray rotateArray = new RotateArray();
-        int array[] = {1,2,3,4,5,6,7};
-
-        rotateArray.leftRotate(array, 3, 7);
-        rotateArray.printArray(array, 7);
-
-    }
-}
-
-
-// Class for Challenge 2
-
-class RotateArray {
-    void leftRotate(int array[], int rotateBy, int size) {
-        for (int index = 0; index < rotateBy; index++) {
-            leftRotateByOne(array, size);
-        }
-    }
-
-    void leftRotateByOne(int array[], int size) {
-        int index;
-        int tempArray;
-        tempArray = array[0];
-        for (index = 0; index < size - 1; index++) {
-            array[index] = array[index + 1];
-        }
-        array[index] = tempArray;
-    }
-
-    void printArray(int array[], int size) {
-        for (int index = 0; index < size; index++) {
-            System.out.println(array[index]);
-        }
     }
 }
--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Overview
 
- Repository covering Java string and array solutions
-
- <strong>Challenge 1</strong> -  Given an input string, reverse the string word by word.
-
- <strong>Challenge 2</strong> - Rotate an array of n elements to the lft by k steps
+ <strong>Challenge</strong> -  Given an input string, reverse the string word by word.
